4187b571454dc52ebe6db07fa3ba6e04d5e36597
Eric Masse <ericmas001@gmail.com>
2018-05-22 20:16:46 -0400

    adding renamelog

---
+++ scripts/renamelog.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import os
+from datetime import datetime
+
+filename = datetime.today().strftime("%Y-%m-%d")
+path_full_logs = "/pics/log.txt"
+path_full_new_name = "/pics/logs/log_{0}.txt"
+os.rename(path_full_logs,path_full_new_name.format(filename))
+
